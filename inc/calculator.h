ec9d08ffc46afb32d161f5c6486e5901ef84c33f
99003178 <75561696+99003178@users.noreply.github.com>
2020-12-06 20:06:09 -0800

    Create calculator.h

---
+++ inc/calculator.h
@@ -0,0 +1,16 @@
+#ifndef __CALCULATOR_H__
+#define __CALCULATOR_H__
+
+#include <stdio.h>
+double add(int ,int);
+int sub(int ,int);
+double mult(int ,int);
+int div(int,int);
+int rev(int);
+void DecToBin();
+//void logo();
+void matrix_sum();
+int fact(int);
+//void inv();
+//void square();
+#endif
